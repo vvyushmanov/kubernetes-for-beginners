b39236a0e3da343c0adb4d3c62bd82e2004ed768
Vadim Yushmanov <v_yushmanov@mail.ru>
2023-08-04 02:40:58 +0400

    modified create-vms

--- 99-misc/create-vms.sh
+++ 99-misc/create-vms.sh
@@ -9,12 +9,12 @@ function create_vm {
     yc compute instance create \
       --name $NAME \
       --hostname $NAME \
-      --zone ru-central1-c \
-      --network-interface subnet-name=default,nat-ip-version=ipv4 \
+      --zone ru-central1-a \
+      --network-interface subnet-name=base_subnet,nat-ip-version=ipv4 \
       --memory 2 \
       --cores 2 \
-      --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2004-lts,type=network-ssd,size=20 \
-      --ssh-key /home/andrey/.ssh/id_rsa.pub
+      --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2204-lts,type=network-ssd,size=20 \
+      --ssh-key $HOME/.ssh/id_rsa.pub
 END
 )
 #  echo "$YC"
